16749a7a9a5a8dce1aaf9ed5a753b6fc6756aa03
Jonathan Reis <jonathan.reis@estudante.ufjf.br>
2023-12-13 20:59:28 -0300

    salva momentaneamente

--- estoque.js
+++ estoque.js
@@ -22,7 +22,7 @@ let estoque = {
             monte = pessoa[i];
             break;
         }
-        if(monte === undefined) {
+        if(!monte ) {
             monte = {'tipo': fruta, 'qtd':0};
             pessoa.push(monte);
         }
@@ -30,6 +30,33 @@ let estoque = {
         return;
        }
     }
+
+    if(destino === "pomar") {
+        const pessoa = estoque[origem];
+        let monte;
+        for (let i=0; i < pessoa.length; i++) {
+            const monte = pessoa[i];
+            if(monte.tipo === fruta){
+                 monte = pessoa[i];
+                 break;
+             }
+    }
+    if (!monte) {
+        return;
+    }
+    monte.qtd -= Math.min(quantidade, monte.qtd);
+   }
+
+ 
+   function dePomarParaPessoa(destino, quantidade, fruta) {
+    const pessoa = estoque[origem];
+    let monte;
+    for (let i=0; i < pessoa.length; i++) {
+        const monte = pessoa[i];
+        if(monte.tipo === fruta){
+             monte = pessoa[i];
+             break;
+         }
    }
    
    // export {getEstoque}
